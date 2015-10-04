59ef1e7794d9381c63b6e636937dec3c428eb3e2
PAVAN KUMAR JALLA <pavankumar.jalla@sjsu.edu>
2015-10-04 00:21:57 -0700

    i pavan deleted these files

--- pay.py
+++
@@ -1,9 +0,0 @@
-hrs = raw_input("Enter Hours:")
-h = float(hrs)
-rate=10.50
-if hrs < 40:
-    pay=hours*rate
-    print "the pay is:", pay
-else:
-    pay = 40 * rate + (h-40)*1.5*rate
-    print "the pay is:", pay
